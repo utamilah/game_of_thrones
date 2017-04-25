606a04fa878e3faa96e68e6a405cc6d9b659ea91
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-25 15:42:36 -0400

    house seed data created

---
+++ db/migrate/20170425191302_create_houses.rb
@@ -0,0 +1,11 @@
+class CreateHouses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :location
+      t.string :img_url
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+house_targaryen = House.create(name: "House Targaryen", location: "Valyria", img_url: "https://vignette2.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest?cb=20161206005534")
+house_tyrell = House.create(name: "House Tyrell", location: "Highgarden", img_url: "https://vignette2.wikia.nocookie.net/gameofthrones/images/c/cf/House-Tyrell-Main-Shield.PNG/revision/latest?cb=20170108163035")
+house_lannister = House.create(name: "House Lannister", location: "Casterly Rock", img_url: "https://vignette3.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest?cb=20170101095357")
+house_stark = House.create(name:"House Stark", location: "Winterfell", img_url: "https://vignette4.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142")
+
+house_arryn = House.create(name: 'House Arryn',location: "The Vale of Arryn", img_url: "https://vignette4.wikia.nocookie.net/gameofthrones/images/1/15/House-Arryn-Main-Shield.PNG/revision/latest?cb=20170101094153")
+house_frey = House.create(name: 'House Frey',location: 'The Riverlands', img_url: "https://vignette1.wikia.nocookie.net/gameofthrones/images/e/e6/House-Frey-Main-Shield.PNG/revision/latest?cb=20160312124244")
+house_greyjoy = House.create(name: 'House Greyjoy',location: 'Iron Islands', img_url: "https://vignette3.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20160312122204")
